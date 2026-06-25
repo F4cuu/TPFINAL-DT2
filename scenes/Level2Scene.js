705d8f8ee87580fcb138e23a1d8ac724f02978fe
F4cuu <f4cundoibanez@gmail.com>
2026-06-24 21:16:08 -0300

    commit-10

--- scenes/Level2Scene.js
+++ scenes/Level2Scene.js
@@ -92,9 +92,9 @@ class Level2Scene extends Phaser.Scene {
       if (600 - p > 0) { const w = this.walls.create(x, p + (600 - p) / 2, 'wall'); w.setDisplaySize(14, 600 - p).refreshBody(); }
     });
     this.seats = this.physics.add.staticGroup();
-    [190, 410, 630].forEach(c => {
+    [[200, 150], [409, 196], [619, 152]].forEach(([c, w]) => {
       [[60,60],[156,60],[252,60],[348,60],[444,60],[540,60]].forEach(([y, h]) => {
-        const s = this.add.rectangle(c, y, 110, h, 0x555555);
+        const s = this.add.rectangle(c, y, w, h, 0x555555);
         this.physics.add.existing(s, true); this.seats.add(s);
       });
     });
@@ -111,11 +111,11 @@ class Level2Scene extends Phaser.Scene {
 
   createCoins() {
     this.coins = this.physics.add.staticGroup();
-    [[410, 108], [605, 108], [180, 204], [410, 300], [605, 300],
-      [180, 396], [410, 492], [605, 492],
-      [300, 60], [520, 60], [300, 156], [520, 156],
-      [300, 252], [520, 252], [300, 348], [520, 348],
-      [300, 444], [520, 444]].forEach(p => {
+    [[293, 108], [525, 108], [293, 204], [525, 204],
+      [293, 300], [525, 300], [293, 396], [525, 396],
+      [293, 60], [525, 60], [293, 156], [525, 156],
+      [293, 252], [525, 252], [293, 348], [525, 348],
+      [293, 444], [525, 444]].forEach(p => {
       const c = this.coins.create(p[0], p[1], 'coin');
       c.setDisplaySize(12, 12);
     });
@@ -149,8 +149,8 @@ class Level2Scene extends Phaser.Scene {
   spawnHazard() {
     if (this.timeRemaining <= 0) return;
     const type = Phaser.Utils.Array.GetRandom(['box', 'luggage', 'person']);
-    const lane = Phaser.Utils.Array.GetRandom([300, 520]);
-    const x = Phaser.Math.Between(-15, 15) + lane;
+    const lane = Phaser.Utils.Array.GetRandom([293, 525]);
+    const x = Phaser.Math.Between(-4, 4) + lane;
     const sz = { box: [30, 30], luggage: [36, 26], person: [20, 34] };
     this.showWarning(x);
     this.time.delayedCall(800, () => {
